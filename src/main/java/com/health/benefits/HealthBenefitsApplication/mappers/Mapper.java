869bcc518cc46893e1617f3477e990b1ff899a17
Kenneth Stallworth <kennethstallworth@hotmail.com>
2025-08-05 23:12:39 -0400

    implementing mappers

---
+++ src/main/java/com/health/benefits/HealthBenefitsApplication/mappers/Mapper.java
@@ -0,0 +1,5 @@
+package com.health.benefits.HealthBenefitsApplication.mappers;
+
+public interface Mapper<A, B> {
+
+}
---
+++ src/main/java/com/health/benefits/HealthBenefitsApplication/mappers/impl/CarrierMapperImpl.java
@@ -0,0 +1,7 @@
+package com.health.benefits.HealthBenefitsApplication.mappers.impl;
+
+import com.health.benefits.HealthBenefitsApplication.mappers.Mapper;
+
+public class CarrierMapperImpl implements Mapper<CarrierEntity, CarrierDTO> {
+
+}
